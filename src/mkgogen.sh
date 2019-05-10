ae1dc67a18641e77a64a193feb11b98fbb755f2e
Owais Lone <hello@owaislone.org>
2019-05-10 13:11:18 +0530

    Generate using gogoproto

--- src/mkgogen.sh
+++ src/mkgogen.sh
@@ -13,17 +13,25 @@
 # ./mkgogen.sh
 
 OUTDIR="$(go env GOPATH)/src"
+INCLUDES="$(go env GOPATH)/src/github.com/gogo/protobuf/protobuf"
 
-protoc --gofast_out=plugins=grpc:$OUTDIR opencensus/proto/stats/v1/stats.proto \
-    && protoc --gofast_out=plugins=grpc:$OUTDIR opencensus/proto/metrics/v1/metrics.proto \
-    && protoc --gofast_out=plugins=grpc:$OUTDIR opencensus/proto/resource/v1/resource.proto \
-    && protoc --gofast_out=plugins=grpc:$OUTDIR opencensus/proto/trace/v1/trace.proto \
-    && protoc --gofast_out=plugins=grpc:$OUTDIR opencensus/proto/trace/v1/trace_config.proto \
-    && protoc -I=. --gofast_out=plugins=grpc:$OUTDIR opencensus/proto/agent/common/v1/common.proto \
-    && protoc -I=. --gofast_out=plugins=grpc:$OUTDIR opencensus/proto/agent/metrics/v1/metrics_service.proto \
-    && protoc -I=. --gofast_out=plugins=grpc:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto \
-    && protoc --gofast_out=plugins=grpc,logtostderr=true,grpc_api_configuration=./opencensus/proto/agent/trace/v1/trace_service_http.yaml:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto
-#    && protoc --grpc-gateway_out=logtostderr=true,grpc_api_configuration=./opencensus/proto/agent/trace/v1/trace_service_http.yaml:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto
+ARGS="Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types,\
+goproto_registration=true"
+
+
+protoc --gogofaster_out=$ARGS,plugins=grpc:$OUTDIR opencensus/proto/stats/v1/stats.proto \
+    && protoc --gogofaster_out=$ARGS,plugins=grpc:$OUTDIR opencensus/proto/metrics/v1/metrics.proto \
+    && protoc --gogofaster_out=$ARGS,plugins=grpc:$OUTDIR opencensus/proto/resource/v1/resource.proto \
+    && protoc --gogofaster_out=$ARGS,plugins=grpc:$OUTDIR opencensus/proto/trace/v1/trace.proto \
+    && protoc --gogofaster_out=$ARGS,plugins=grpc:$OUTDIR opencensus/proto/trace/v1/trace_config.proto \
+    && protoc -I=. --gogofaster_out=$ARGS,plugins=grpc:$OUTDIR opencensus/proto/agent/common/v1/common.proto \
+    && protoc -I=. --gogofaster_out=$ARGS,plugins=grpc:$OUTDIR opencensus/proto/agent/metrics/v1/metrics_service.proto \
+    && protoc -I=. --gogofaster_out=$ARGS,plugins=grpc:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto \
+    && protoc --grpc-gateway_out=logtostderr=true,grpc_api_configuration=./opencensus/proto/agent/trace/v1/trace_service_http.yaml:$OUTDIR opencensus/proto/agent/trace/v1/trace_service.proto
 
 # Generate OpenApi (Swagger) documentation file for grpc-gateway endpoints.
 OPENAPI_OUTDIR=../gen-openapi
